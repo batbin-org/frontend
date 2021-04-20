0bdd2272db399e97ecde60b9c9bc344f9d3cab33
Udit Karode <udit.karode@gmail.com>
2021-04-20 17:32:40 +0530

    pages: index: set theme-color for phones

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,21 @@ import React, { useRef } from "react";
 import TopBar from "../components/TopBar";
 import Editor from "../components/Editor";
 import axios from 'axios';
+import Head from 'next/head';
 
 function App() {
   const editorRef = useRef(null);
 
   return (
     <div style={{ display: "flex", flexDirection: "column", overflowX: 'hidden', height: "100%" }}>
+      <Head>
+          {/* Chrome, Firefox OS and Opera */}
+          <meta name="theme-color" content="#000000"/>
+          {/* Windows Phone */}
+          <meta name="msapplication-navbutton-color" content="#000000"/>
+          {/* iOS Safari */}
+          <meta name="apple-mobile-web-app-status-bar-style" content="#000000"/>
+      </Head>
       <TopBar buttonText="Save" buttonAction={async () => {
           try {
             const form = new FormData();
