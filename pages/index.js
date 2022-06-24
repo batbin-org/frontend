643c25f03cde47f8ad7a6d63459c0c81629cacea
alok8bb <alok8bb@gmail.com>
2022-06-24 09:03:10 +0530

    [misc:app] add favicons

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,12 @@ function App() {
         <meta name="msapplication-navbutton-color" content="#000000" />
         {/* iOS Safari */}
         <meta name="apple-mobile-web-app-status-bar-style" content="#000000" />
+
+        {/* Favicons */}
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       </Head>
       <TopBar
         buttonText="Save"
